dd7e0e2b08bbfd11938eafdd5da0f55124d189a2
alenafedyaeva <291407@gmail.com>
2020-10-20 06:42:07 +0300

    fixed reviewed

--- homework1/task1/main.go
+++ homework1/task1/main.go
@@ -9,28 +9,27 @@ import (
 	"time"
 )
 
+func findStrInURL(subsrt string, url string) bool {
+	rez := false
+	b, body := readURL(url)
 
-func findStrInUrl(subsrt string,url string) bool{
-	rez:=false;	
-	b, body:=readUrl(url)
-
-	if(b){
+	if b {
 		//fmt.Println(body)
-		rez=find(subsrt,body)
+		rez = find(subsrt, body)
 	}
-	return rez;
+	return rez
 }
 
 // Поиск подстроки в строке (* переписать функцию на поиск в массиве)
-func find(subsrt string,str string) bool{
-	res:=strings.Contains(str,subsrt)
+func find(subsrt string, str string) bool {
+	res := strings.Contains(str, subsrt)
 	return res
 }
 
 //Чтение body в string
-func readUrl(url string)(bool,string){
-	rezStr:=""
-	b:=false;
+func readURL(url string) (bool, string) {
+	var rezStr string
+	var b bool
 	httpCli := &http.Client{
 		Timeout: time.Second * 10,
 	}
@@ -41,36 +40,35 @@ func readUrl(url string)(bool,string){
 	resp, err := httpCli.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return b,rezStr
+		return b, rezStr
 	}
-	
+
 	defer resp.Body.Close()
 
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-			return b,rezStr
-		}
-		b=true
-		rezStr = string(bodyBytes)
+	if resp.StatusCode != http.StatusOK {
+		//log.Fatal(errors.New("readURL: status code not StatusOK "))
+		return b, rezStr
 	}
-	return b,rezStr
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return b, rezStr
+	}
+	return true, string(bodyBytes)
 }
-
 
 func main() {
 	var urlArray [3]string
-	urlArray[0]="https://mail.ru"
-	urlArray[1]="https://golang.org/"
-	urlArray[2]="https://google.com"
-	
-	substr:="Go is an open source programming "
+	urlArray[0] = "https://mail.ru"
+	urlArray[1] = "https://golang.org/"
+	urlArray[2] = "https://google.com"
 
-	for _, url := range urlArray{
-		rez:=findStrInUrl(substr,url)
-		if(rez){
-			fmt.Printf("Find in page :  ",url)
+	substr := "Go is an open source programming "
+
+	for _, url := range urlArray {
+		rez := findStrInURL(substr, url)
+		if rez {
+			fmt.Printf("Find in page :  %s", url)
 		}
 	}
 	fmt.Println("\nbye")
